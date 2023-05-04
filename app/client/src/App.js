bb85b4d92e057237b238e88034b5f000a60314f9
Joymfl <jmjoymallik@gmail.com>
2023-05-04 07:10:47 +0530

    Add functionality for reading data

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { PublicKey } from "@solana/web3.js";
+import { PublicKey, AccountInfo } from "@solana/web3.js";
 
 function App() {
   const [backEndData, setBackendData] = useState([{}]);
   const [accountData, setAccountData] = useState({});
+  const [accountInfo, setAccountInfo] = useState({});
   const [tx, setTx] = useState("");
   useEffect(() => {
     fetch("/api")
@@ -18,10 +19,18 @@ function App() {
       .then((res) => res.json())
       .then((data) => setAccountData(data));
   }, []);
+
   const onCLick = () => {
     fetch("/airdrop", {
       method: "POST",
     });
+  };
+  const getData = () => {
+    fetch("/getInfo")
+      .then((res) => res.json())
+      .then((data) => {
+        setAccountInfo(data);
+      });
   };
   function init() {
     fetch("/init", { method: "POST" });
@@ -44,6 +53,12 @@ function App() {
       </div>
       <div>
         <button onClick={init}>Init Program</button>
+      </div>
+      <div>
+        <button onClick={getData}>Get Data</button>
+      </div>
+      <div>
+        <p>{JSON.stringify(accountInfo.data.body)}</p>
       </div>
     </div>
   );
--- app/server/server.js
+++ app/server/server.js
@@ -4,12 +4,16 @@ import {
   Connection,
   LAMPORTS_PER_SOL,
   SystemProgram,
+  Account,
 } from "@solana/web3.js";
 import idl from "./idl/test_web_app.json" assert { type: "json" };
+import { deserialize } from "borsh";
 
 import express from "express";
 const app = express();
 import { AnchorProvider, Program, Wallet } from "@project-serum/anchor";
+import * as borsh from "borsh";
+import { struct } from "buffer-layout";
 
 const wallet = Keypair.generate();
 let ANCHOR_WALLET = new Wallet(wallet);
@@ -18,10 +22,27 @@ const programId = new PublicKey("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 const connection = new Connection("http://localhost:8899", "confirmed");
 const provider = new AnchorProvider(connection, ANCHOR_WALLET, "confirmed");
 const program = new Program(idl, programId, provider);
+let tweetAccountIDL = idl.accounts["tweet"];
+let accounts = idl.accounts["name"];
 const tweetAccount = PublicKey.findProgramAddressSync(
   [wallet.publicKey.toBuffer()],
   programId
 );
+const schema = new Map([
+  [
+    Account,
+    {
+      kind: "struct",
+      fields: [
+        ["discriminator", "u8"],
+        ["title", "string"],
+        ["body", "string"],
+        ["count", "u64"],
+      ],
+    },
+  ],
+]);
+
 app.get("/api", (req, res) => {
   res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
@@ -50,6 +71,15 @@ app.post("/init", (req, res) => {
     .signers([wallet])
     .rpc();
   // .then((r) => console.log(r));
+});
+app.get("/getInfo", async (req, res) => {
+  // let accountInfo = await provider.connection
+  //   .getParsedAccountInfo(tweetAccount[0])
+  //   .then((res) => borsh.deserialize(schema, struct, res.value));
+  let accountInfo = await program.account.tweet.fetch(tweetAccount[0]);
+  res.json({
+    data: accountInfo,
+  });
 });
 
 app.listen(5000, () => {
